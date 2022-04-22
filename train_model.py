5d35e4892bd1f47524c973ffdd10916e78c22b2b
isaacgoff <isaac.goff@colorado.edu>
2022-04-22 17:59:25 -0600

    Debug

--- train_model.py
+++ train_model.py
@@ -85,7 +85,8 @@ def main():
             tng_loss.backward()
             optimizer.step()
             epoch_tng_loss += float(tng_loss.detach().item())
-            epoch_tng_score += (predicted_labels.argmax(axis=1) == label_batch).sum().detach.item()
+            print(f'{epoch_tng_score.shape}')
+            epoch_tng_score += (predicted_labels.argmax(axis=1) == label_batch.argmax(axis=1)).sum().detach.item()
             n += len(label_batch)
 
         print(f'\nn = {n}')
