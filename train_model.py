b7881074876e8f502ebbd9ce96d06187c90f1d21
isaacgoff <isaac.goff@colorado.edu>
2022-04-22 11:25:43 -0600

    debug

--- train_model.py
+++ train_model.py
@@ -40,10 +40,11 @@ def main():
     # val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=False)
 
     # Test dataloader function
-    img, label = next(iter(tng_dataloader))
+    img = next(iter(tng_dataloader))
     plt.imshow(img)
     plt.show()
-    print(f'label:{label}\nimg:\n{img}')
+    # print(f'label:{label}\nimg:\n{img}')
+    print(f'img:\n{img}')
 
     end = datetime.now()
     print(f'\nelapsed time: {end - start}')
