ef5ca70b9c88ea847f84f33fa31c2b4799db38b6
isaacgoff <isaac.goff@colorado.edu>
2022-04-22 11:17:26 -0600

    debug

--- create_dataset.py
+++ create_dataset.py
@@ -30,7 +30,7 @@ def create_dataset(audio_input_path, json_path):
 
     data_np = torch.tensor(np.stack(data))
     labels = F.one_hot(torch.tensor(np.stack(labels)), num_classes=11)
-    print(f'data_np:\n{data_np}\nlabels:\n{labels}')
+    print(f'data_np ({data_np.shape}):\n{data_np}\nlabels ({labels.shape}):\n{labels}')
     return AudioSpectogramDataset(data_np, labels)
 
 
