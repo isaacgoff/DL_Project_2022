97270e3a53d80fe15134d57e595c9ed9d5f18d1b
kylenannig <71528170+kylenannig@users.noreply.github.com>
2022-04-24 17:38:07 -0600

    changed deafult back

--- test_model.py
+++ test_model.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 def main():
     parser = argparse.ArgumentParser(description='Test the individual instrument identification model')
     parser.add_argument('-f')  # Required for argument parser to work in Colab
-    parser.add_argument('--test_folder', type=str, default='debug/')
+    parser.add_argument('--test_folder', type=str, default='audio/')
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--model_name', type=str,  default='billy_joel_cnn')
     parser.add_argument('--model_type', type=str, default='Basic_4_Layer_CNN')
