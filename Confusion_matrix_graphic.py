9e7e1281e957d7d16708a3ec62070bef41adad49
isaacgoff <isaac.goff@colorado.edu>
2022-04-25 16:08:16 -0600

    Debug overlapping figures

--- Confusion_matrix_graphic.py
+++ Confusion_matrix_graphic.py
@@ -10,17 +10,17 @@ def plot_confusion_matrix(cm, fig, ax, title='Confusion matrix', cmap=plt.cm.Blu
     classes = ('bass', 'brass', 'flute', 'guitar', 'keyboard', 'mallet', 'organ', 'reed', 'string', 'vocal')
     # plt.figure(1, figsize=(11,11))
     ax.imshow(cm, interpolation='nearest', cmap=cmap)
-    ax.title(title)
+    ax.set_title(title)
     ax.colorbar()
     tick_marks = np.arange(len(classes))
-    ax.xticks(tick_marks, classes, rotation=45)
-    ax.yticks(tick_marks, classes)
+    ax.set_xticks(tick_marks, classes, rotation=45)
+    ax.set_yticks(tick_marks, classes)
 
     thresh = 0.5
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        ax.text(j, i, "{:.3f}".format(cm[i, j]), horizontalalignment="center",
+        ax.set_text(j, i, "{:.3f}".format(cm[i, j]), horizontalalignment="center",
                            color="white" if cm[i, j] > thresh else "black")
 
-    ax.tight_layout()
-    ax.ylabel('True label')
-    ax.xlabel('Predicted label')
+    fig.tight_layout()
+    ax.set_ylabel('True label')
+    ax.set_xlabel('Predicted label')
