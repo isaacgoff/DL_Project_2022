19a3b9fee3af135853cbc2c39bd03618d4b7c6f0
Nimish Bhide <38682357+nimbid@users.noreply.github.com>
2022-04-25 22:46:39 -0600

    debug plotting

--- train_model.py
+++ train_model.py
@@ -177,7 +177,8 @@ def main():
     print(f'\nBest Epoch: {best_epoch}')
     # print(f'Training Loss = {epoch_results["tng_loss"]} // Training Acc = {epoch_results["tng_acc"]} '
     #       f'// Validation Acc = {epoch_results["val_acc"]}')
-    plot_model_results(epoch_results)
+    # plot_model_results(epoch_results)
+    plot_test(epoch_results)
 
     # Save the best model state for future use
     if save_trained_model:
